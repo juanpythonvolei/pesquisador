e042093cb342e4b1349d8a909b80f3c1bf4fbb9c
juanpythonvolei <juanzsalca@outlook.com>
2024-10-18 00:11:03 -0300

    Update consulta.py

--- pages/consulta.py
+++ pages/consulta.py
@@ -8,18 +8,18 @@ usuarios = [elemento.nome for elemento in session.query(Usuario).all()]
 temas = [item.tema for item in session.query(Pesquisa).all()]
 
 selecao_usuario = st.selectbox(label="Selecione seu usuário",options=usuarios,index=None)
-selecao_tema = st.selectbox(label="Selecione seu tema",options=temas,index=None)
+selecao_tema = st.selectbox(label="Selecione seu tema",options=[item.tema for item in session.query(Pesquisa).filter(Pesquisa.usuario == selecao_usuario).all()],index=None)
 
+if selecao_usuario and selecao_tema:
+    pesquisas = query_search(selecao_usuario,selecao_tema)
 
-pesquisas = query_search(selecao_usuario,selecao_tema)
-
-col1,col2  = st.columns(2)
-with col1:
-    if len(pesquisas) > 0:
-        st.metric(value=len(pesquisas),label="Total de pesquisas para esse tema")
-    else:
-        st.info("Usuário não realizou pesquisas sobre esse tema")
-with col2:
-    st.metric(label="Total de pesquisas do usuário",value=len([item for item in session.query(Pesquisa).filter(Pesquisa.usuario == selecao_usuario)]))
-for pesquisa in pesquisas:
-    st.info(pesquisa)
+    col1,col2  = st.columns(2)
+    with col1:
+        if len(pesquisas) > 0:
+            st.metric(value=len(pesquisas),label="Total de pesquisas para esse tema")
+        else:
+            st.info("Usuário não realizou pesquisas sobre esse tema")
+    with col2:
+        st.metric(label="Total de pesquisas do usuário",value=len([item for item in session.query(Pesquisa).filter(Pesquisa.usuario == selecao_usuario)]))
+    for pesquisa in pesquisas:
+        st.info(pesquisa)
